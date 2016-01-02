bfdd93d5e60e9188a30b348f8696a29462ee7254
KwonNam Son <kwon37xi@gmail.com>
2016-01-02 13:43:01 +0900

    add String.format based example

--- src/test/java/kr/pe/kwonnam/dynamicql/stringbuilderqlparams/DynamicQlParamsQueryBuildTest.java
+++ src/test/java/kr/pe/kwonnam/dynamicql/stringbuilderqlparams/DynamicQlParamsQueryBuildTest.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 
+import static java.lang.String.format;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class DynamicQlParamsQueryBuildTest extends AbstractQueryBuildTest {
@@ -25,17 +26,30 @@ public class DynamicQlParamsQueryBuildTest extends AbstractQueryBuildTest {
             .append("WHERE 1 = 1\n");
 
         if (user.getUserId() != null) {
-            builder.append("AND user_id = ").append(dqp.param(user.getUserId())).append("\n");
+            builder.append(format("AND user_id = %s %n", dqp.param(user.getUserId())));
         }
         if (StringUtils.isNotEmpty(user.getName())) {
-            builder.append("AND name = ").append(dqp.param(user.getName())).append("\n");
+            builder.append(format("AND name = %s %n", dqp.param(user.getName())));
         }
         if (user.getBirthday() != null) {
-            builder.append("AND birthday = ").append(dqp.param(user.getBirthday())).append("\n");
+            builder.append(format("AND birthday = %s %n", dqp.param(user.getBirthday())));
         }
         if (CollectionUtils.isNotEmpty(zipCodes)) {
-            builder.append("AND zip_code in (").append(dqp.inParams(zipCodes)).append(")\n");
+            builder.append(format("AND zip_code in (%s) %n",dqp.inParams(zipCodes)));
         }
+
+//        if (user.getUserId() != null) {
+//            builder.append("AND user_id = ").append(dqp.param(user.getUserId())).append("\n");
+//        }
+//        if (StringUtils.isNotEmpty(user.getName())) {
+//            builder.append("AND name = ").append(dqp.param(user.getName())).append("\n");
+//        }
+//        if (user.getBirthday() != null) {
+//            builder.append("AND birthday = ").append(dqp.param(user.getBirthday())).append("\n");
+//        }
+//        if (CollectionUtils.isNotEmpty(zipCodes)) {
+//            builder.append("AND zip_code in (").append(dqp.inParams(zipCodes)).append(")\n");
+//        }
         builder.append("LIMIT ").append(dqp.param(10));
 
         log.info("StringBuilder with DynamicQlParams : {}", builder.toString());
